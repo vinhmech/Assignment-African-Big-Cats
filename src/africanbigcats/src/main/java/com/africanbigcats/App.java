8d9be3fd91250132181ebea560b44817f5f06676
vinhmech <vincenguyen262@gmail.com>
2021-10-03 16:52:02 -0700

    Got example code working as-is and running in maven project

--- src/africanbigcats/src/main/java/com/africanbigcats/App.java
+++ src/africanbigcats/src/main/java/com/africanbigcats/App.java
@@ -1,5 +1,7 @@
 package com.africanbigcats;
 
+import java.util.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Menu appMenu = new Menu();
+
+        Character command = '_';
+        LinkedList<Panthera> catList = new LinkedList<>(); 
+
+        // loop until user quits
+        while (command != 'q') {
+
+            // print the menu
+            appMenu.print();
+
+            // get a command
+            System.out.print("Enter a command: ");
+            command = appMenu.getCommand();
+
+            // execute a commmand
+            appMenu.executeCommand(command, catList);
+
+            // move the cats to new positions in africa
+            appMenu.update(catList);
+
+        }
+
     }
 }
